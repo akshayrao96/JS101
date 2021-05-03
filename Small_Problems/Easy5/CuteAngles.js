3a1872795439184d771c17216765b510b3b32952
Akshay <akshay96@uw.edu>
2021-05-03 09:54:51 -0700

    fixed CuteAngles

--- Small_Problems/Easy5/CuteAngles.js
+++ Small_Problems/Easy5/CuteAngles.js
@@ -1,22 +1,26 @@
-function getMinutes(num) {
-
+function gettingNum(num) {
+  return num - Math.floor(num);
 }
 
-function getSeconds(num) {
-
+function getTime(num) {
+  return num * 60;
 }
 
 function dms(num) {
-  let str = String(Math.floor(num)) + "";
-
+  let firstStr = String(Math.floor(num)) + "˚";
+  let num1 = gettingNum(num);
+  let secondStr = String(Math.floor(getTime(num1))) + "'";
+  let num2 = gettingNum(getTime(num1));
+  let thirdStr = String(Math.floor(getTime(num2))) + '"';
+  return firstStr + secondStr + thirdStr;
 }
 
 
 
 
-dms(30); // 30°00'00"
-dms(76.73); // 76°43'48"
-dms(254.6); // 254°35'59"
-dms(93.034773); // 93°02'05"
+console.log(dms(30)); // 30°00'00"
+console.log(dms(76.73)); // 76°43'48"
+console.log(dms(254.6)); // 254°35'59"
+console.log(dms(93.034773)); // 93°02'05"
 dms(0); // 0°00'00"
 dms(360); // 360°00'00" or 0°00'00"
